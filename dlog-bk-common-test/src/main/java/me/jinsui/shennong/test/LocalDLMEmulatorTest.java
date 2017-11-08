d19d3582f1a1d938318d45c49644c069331ddc5a
Arvin <arvindevel@gmail.com>
2017-11-08 10:05:52 +0800

    make module structure more clean

--- dlog-bk-common-test/src/main/java/me/jinsui/shennong/test/LocalDLMEmulatorTest.java
+++ dlog-bk-common-test/src/main/java/me/jinsui/shennong/test/LocalDLMEmulatorTest.java
@@ -5,10 +5,7 @@ import org.apache.bookkeeper.shims.zk.ZooKeeperServerShim;
 import org.apache.bookkeeper.util.IOUtils;
 //import dlshade.org.apache.bookkeeper.util.LocalBookKeeper;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.distributedlog.DLMTestUtil;
 import org.apache.distributedlog.LocalDLMEmulator;
-import org.apache.distributedlog.TestAsyncReaderWriter;
-import org.apache.distributedlog.TestDistributedLogBase;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -19,7 +16,7 @@ import java.io.File;
  * Test LocalDLMEmulator & TestDistributedLogBase
  * ISSUE:
  * After Dlog bump bk version to 4.6, the LocalDLMEmulator in shaded/bkshade jar can't start bk,
- * we can set dlog in the pom.xml
+ * we can set dlog version in the pom.xml
  */
 public class LocalDLMEmulatorTest {
 
@@ -31,8 +28,6 @@ public class LocalDLMEmulatorTest {
     public static void main(String[] args) throws Exception {
 
         testLocalDLMEmulator();
-//        testDLMTestBase();
-//        testLocalBookkeeper();
 
     }
 
@@ -44,12 +39,6 @@ public class LocalDLMEmulatorTest {
      *  it's ok previously because MathUtil in bk is not refactored and the dlog use bk4.5
      */
     private static void testLocalDLMEmulator() throws Exception {
-        // compiled, target file path
-        // System.out.println(LocalDLMEmulatorTest.class.getClassLoader().getResource("").getPath());
-        //todo how to specify a valid path
-        //   PropertyConfigurator.configure("../conf/log4j.properties");
-        // use default config temporally, now use resources directory to put log4j config
-        //BasicConfigurator.configure();
         LocalDLMEmulator localDLMEmulator = LocalDLMEmulator.newBuilder()
                 .numBookies(1)
                 .zkHost("127.0.0.1")
@@ -61,20 +50,5 @@ public class LocalDLMEmulatorTest {
         System.out.println("localDLMEmulator.start finish");
     }
 
-
-
-    private static void testDLMTestBase() throws Exception {
-//        BasicConfigurator.configure();
-        TestDistributedLogBase.setupCluster();
-        System.out.println("setupCluster finish");
-
-    }
-
-    // to check whether the problem is related to LocalBookkeeper
-//    private static void testLocalBookkeeper() throws Exception {
-//        BasicConfigurator.configure();
-//        LocalBookKeeper.startLocalBookies("127.0.0.1", 2181,
-//                3, true, 5000, new ServerConfiguration().setAllowLoopback(true));
-//    }
 }
 
